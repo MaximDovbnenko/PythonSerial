2d7a07cc4cf4e8664c52fbaa0e83febfbc572816
Максим Довбненко < >
2017-04-03 20:41:08 +0300

    Master add main

--- SerialReadTest.py
+++ SerialReadTest.py
@@ -1,10 +1,22 @@
-import serial
+'''
+/*Arduino code*/
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(8, INPUT);
+  pinMode(9, INPUT);
+  Serial.begin(115200);
+}
 
-ser = serial.Serial("COM3", 115200)
-DataArrayString = []
-DataArray = []
-maxCount = 100
-EPS = 2
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print(digitalRead(8));
+  Serial.print(":");
+  Serial.print(digitalRead(9));
+  Serial.print(":");
+  Serial.println(millis());
+}
+'''
+import serial
 
 class DataLine:
     def __init__(self, d1, d2, time):
@@ -19,47 +31,53 @@ class DataLine:
     def __str__(self):
         return "{"+self.D1+","+self.D2+","+self.Time+"}"
 
-def get_data():
-    count = 0
-    DataArrayString.clear()
-    while count < maxCount:
-        data = read_serial()
-        DataArrayString.append(data)
-        count += 1
-        
-def read_serial():
-    data = ser.readline()
-    data = str(data)
-    data = data[2:]
-    data = data.replace("\\r\\n'", '')
-    return data
+class DataGather:
+    def __init__(self, comPort, rate, maxCount, s_val):
+        self.DataArrayString = []
+        self.serial = serial.Serial(comPort, rate)
+        self.DataArray = []
+        self.maxCount = maxCount
+        self.Val = s_val
 
-def calculate():
-    DataArray.clear()
-    for line in DataArrayString:
-        split_data = line.split(':')
-        if len(split_data) == 3:
-            tmp_data = DataLine(split_data[0], split_data[1], split_data[2])
-            DataArray.append(tmp_data)
-    IndexD1 = 0
-    IndexD2 = 0
-    i = 0
-    for line in DataArray:
-        if line.D1 == 0: IndexD1 = i
-        else: i += 1
-    i = 0
-    for line in DataArray:
-        if line.D2 == 0: IndexD2 = i
-        else: i += 1
-    ResultTime = DataArray[IndexD2].Time - DataArray[IndexD1].Time
-    return ResultTime
+    def get_data(self):
+        count = 0
+        self.DataArrayString.clear()
+        while count < self.maxCount:
+            data = self.read_serial()
+            self.DataArrayString.append(data)
+            count += 1
         
-while True:
-    get_data()
-    dT = calculate()
-    print(dT)
-    direction = -1 if dT < 0 else 1
-    dT = abs(dT)
-    Error = EPS - dT
-    pass
+    def read_serial(self):
+        data = self.serial.readline()
+        data = str(data)
+        data = data[2:]
+        data = data.replace("\\r\\n'", '')
+        return data
+
+    def calculate(self):
+        self.get_data()
+        self.DataArray.clear()
+        for line in self.DataArrayString:
+            split_data = line.split(':')
+            if len(split_data) == 3:
+                tmp_data = DataLine(split_data[0], split_data[1], split_data[2])
+                self.DataArray.append(tmp_data)
+        IndexD1 = 0
+        IndexD2 = 0
+        i = 0
+        for line in self.DataArray:
+            if line.D1 == self.Val: 
+                IndexD1 = i
+                break
+            i += 1
+        i = 0
+        for line in self.DataArray:
+            if line.D2 == self.Val: 
+                IndexD2 = i
+                break
+            i += 1
+        ResultTime = self.DataArray[IndexD1].Time - self.DataArray[IndexD2].Time
+        return ResultTime
+        
+
 
---
+++ main.py
@@ -0,0 +1,9 @@
+from SerialReadTest import DataGather
+
+dataGather = DataGather('COM3', 115200, 100, 0)
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
