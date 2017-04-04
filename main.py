932767859fe85995d71262edcb865212df33518a
Максим Довбненко < >
2017-04-04 21:06:35 +0300

    Develop commit test

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from SerialReadTest import DataGather
+import numpy as np
+#dataGather = DataGather('COM3', 115200, 100, 0)
 
-dataGather = DataGather('COM3', 115200, 100, 0)
-
+print(a)
 def main():
+    #dT = dataGather.calculate()
     pass
 
 if __name__ == '__main__':
